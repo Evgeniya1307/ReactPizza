46c35d88eb7ea57979ad7c0e374a284ae8959e7e
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-06-28 11:55:47 +0400

    setIsLoading(true); // перед загрузкой идёт

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = useState(0);
+
+function Categories({value, onChangeCategory}) { //value -активный индекс храню в велью а onClickCategory
   const categories = [
     "Все",
     "Мясные",
@@ -14,13 +13,12 @@ function Categories() {
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, i) => (
+        {categories.map((categoryName, i) => (
           <li
             key={i}
-            onClick={() => setActiveIndex(i)}
-            className={activeIndex === i ? "active" : ""}
-          >
-            {value}
+            onClick={() => onChangeCategory(i)} 
+            className={value === i ? "active" : ""}>
+{categoryName}
           </li>
         ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,20 +8,27 @@ import Categories from "../components/Categories";
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState(0);
+  
   React.useEffect(() => {
-    fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items")
+    setIsLoading(true); // перед загрузкой идёт
+    fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items?category=" + categoryId)
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
-        setIsLoading(false);
+        setIsLoading(false);//после загрузки
       });
       window.scrollTo(0,0);//js делаю скрол вверх
-  }, []);
+  }, [categoryId]);
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+      <Categories
+      value={categoryId}
+      onChangeCategory={(i) => setCategoryId(i)}
+    /> {/*// в онклик передаю фу-ию  когда ты сработаешь onChangeCategory={(i) то вызови мне  setCategoryId=(i) */}
+    <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
