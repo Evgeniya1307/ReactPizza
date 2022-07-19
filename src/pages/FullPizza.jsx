2230a97521a200280e9ad066c0835996a52ba780
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-07-19 12:25:26 +0400

    if(pizza) { // проверка нужно ли из пиццы вытащить что то

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import axios from 'axios';
-import { fetchPizzas } from '../redux/slices/pizzaSlice';
+
+
 //инфа по пицце
 
 const FullPizza = () => {
     const [pizza, setPizza]=React.useState();
     const {id} = useParams();
 
-    React.useEffect(() => {
+    React.useEffect(() => { // внутри со-ю асинхрон фу-ию и её вызываю
 async function fetchPizza(){
     try{
     const {data} = await axios.get("https://62b41f5aa36f3a973d2c669d.mockapi.io/items/"+id)
@@ -21,16 +22,15 @@ fetchPizza();
 
 },[])
 
+if(pizza) { // проверка нужно ли из пиццы вытащить что то
+    return "Загрузка...";
+}
+
   return (
     <div className='container'>
-    <img src=""/>
- <h2>
- {id}
- </h2>
- <p>
- lorem ipsum dolor sit amet, consectetur adip
-</p>
-<h4>250 ₽</h4>
+    <img src = {pizza.imageUrl}/>
+ <h2>{pizza.title}</h2>
+<h4>{pizza.price}₽</h4>
     </div>
   )
 }
