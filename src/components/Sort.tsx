09838965784d53a2444fdc65aaca03e7dac44740
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-07-21 11:55:27 +0400

    const sortRef = useRef<HTMLDivElement>(null);//null так ref ожидает получить или null или какой тоэлемент

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,54 +1,62 @@
-import React from "react";
+import { FC, memo, MouseEvent, useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux/es/exports";
-import {useEffect, useRef} from "react";
-import {selectSort, setSort} from "../redux/slices/filterSlice.jsx.js"
+import {setSort} from "../redux/slices/filterSlice.jsx.js"
+import { Sort as SortType, SortPropertyEnum } from ""
 
-
-// создала свой тип 
+// создала свой тип объект содержащий name,sortProperty
 type SortItem = {
   name: string;
-  sortProperty: string;
+  sortProperty: SortPropertyEnum;
 };
 
+type PopupClick = MouseEvent<HTMLBodyElement> & {
+  path: Node[];
+};
+type TSortPopupProps = {
+  value: TSort;
+
   //для списка по популрности
-  export const sortList = [
+  export const sortList: SortItem[]  = [
     { name: "популярности(DESC)", sortProperty: "rating" }, // убывание от большему к меньшему
     { name: "популярности(ASC)", sortProperty: "-rating" }, //-возрастанию от меньшему к большему
     { name: "цене(DESC)", sortProperty: "price" },
     { name: "цене(ASC)", sortProperty: "-price" },
-    { name: "алфавиту(DESC)", sort: "title" },
-    { name: "алфавиту(ASC)", sort: "-title" }
+    { name: "алфавиту(DESC)", sortProperty: "title" },
+    { name: "алфавиту(ASC)", sortProperty: "-title" }
   ];
 
 
 
 
-export const  Sort=()=> {
+  export const Sort: React.FC<SortPopupProps> = React.memo(({ value }) => {
   const dispatch= useDispatch();// будет передвать в редакс действие
- const sort = useSelector(selectSort)// передала фу-ию соз-ую в filterslice стейт из редюсера фильт вытащи свойство стор
- const sortRef = useRef();// ссылка на домэлемент
+  const sortRef = useRef<HTMLDivElement>(null);//null так ref ожидает получить или null или какой тоэлемент  ссылка на домэлемент
  
+
+
+
  const [open, setOpen] = React.useState(false); // переключатель
 
 
-  const onClickListItem = (obj) => {
+  const onClickListItem =(obj: SortItem) => {
    dispatch(setSort(obj))// вернёт объект
     // onChangeSort(i); // когда выберешь какой то сорт
     setOpen(false); // и скройся
   };
 
   useEffect(() => {
-    const handleClickOutside = (event) => {  // храню ссылку внутри этой переменной
-      if (!event.path.includes(sortRef.current)) { // если в эвенте пас не было клика на наш сорт то скрываем окошко  
+    const handleClickOutside = (event: MouseEvent) => {  // храню ссылку внутри этой переменной
+      const _event = event as PopupClick;
+      if (sortRef.current && !_event.path.includes(sortRef.current)) {
         setOpen(false);
       }
     };
-    document.body.addEventListener("click", handleClickOutside); //  document.body.addEventListener("click", делает клик то передай ниже фу-ию
-    return () => document.body.removeEventListener("click", handleClickOutside);//если компонент уд-ся со стр то удаляю обработчик события с body на клик, т,е было размонтирование и вызови эту фу-ию
+    document.body.addEventListener('click', handleClickOutside); //  document.body.addEventListener("click", делает клик то передай ниже фу-ию
+    return () => document.body.removeEventListener('click', handleClickOutside);//если компонент уд-ся со стр то удаляю обработчик события с body на клик, т,е было размонтирование и вызови эту фу-ию
   }, []);
 
   return (
-    <div ref={sortRef} className="sort"> 
+    <div ref={sortRef} className="sort">
       <div className="sort__label">
         {/*сортировка по*/}
         <svg
@@ -64,7 +72,7 @@ export const  Sort=()=> {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span onClick={() => setOpen(!open)}>{sort.name}</span>
+        <span onClick={() => setOpen(!open)}>{value.name}</span>
         {/*когда буду кликать будет скрываться или показываться*/}
       </div>
       {open && ( // будет показываться
@@ -73,14 +81,9 @@ export const  Sort=()=> {
           <ul>
           {sortList.map((obj, i) => (
             <li
-              key={i}
-              onClick={() => {
-                onClickListItem(obj);
-              }}
-                  className={
-                    sort.sortProperty === obj.sortProperty ? "active" : ""
-                  }
-                >
+            key={i}
+            onClick={() => onClickListItem(obj)}
+              className={value.sortProperty === obj.sortProperty ? 'active' : ''}>
                   {/*проверяю то что у родителя хранится в home сравниваю с тем что рендарю */}
                   {obj.name}
                 </li>
@@ -90,9 +93,9 @@ export const  Sort=()=> {
       )}
     </div>
   );
-};
+});
 
-export default Sort;
+
 
 //&& если правая сторона true иди ко второй и верни
 //5&&6 вернёт 6 //0&&3 вернёт 0
