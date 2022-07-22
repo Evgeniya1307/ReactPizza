90271def4e620806fee154c20c9fca77cf73d981
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-07-22 14:50:02 +0400

    RootState = ReturnType<typeof store.getState>; // RootState я глобальный стейт в котором содержится типизация фильтрация корзины пиццы

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -16,7 +16,7 @@ export const store = configureStore({
 
 // главный state(получение типа всего хранилища) c помощью typeof говорим дай мне тип store
 // если нужно из функции вытащить только её содержимое для этого есть - ReturnType- это спец тип он берёт содержимое  и привращает в ТИП
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>; // RootState я глобальный стейт в котором содержится типизация фильтрация корзины пиццы
 
  //превращаю js функцию в тип  и сохрани её в переменную
 export type AppDispatch = typeof store.dispatch;
